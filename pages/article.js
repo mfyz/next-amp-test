982f210c68051abea869da454f428397a944b315
Mehmet Fatih Yildiz <fatih@mfyz.com>
2021-05-22 11:56:58 -0400

    initial commit

--- pages/article.js
+++ pages/article.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { useAmp } from 'next/amp'
 
 export const config = {
-	amp: true,
+	amp: 'hybrid',
 }
 
 export default function Home() {
@@ -15,17 +15,33 @@ export default function Home() {
 			</Head>
 
 			<main>
-				<h1>Next AMP Test</h1>
+				<h1>Next AMP Test / Article Page</h1>
 
-				<a href={isAmp ? '/article?amp=1' : '/article'}>Article Page</a>
+				<a href={isAmp ? '/article' : '/article?amp=1'}>
+					{isAmp ? 'View Non-AMP' : 'View AMP'} Version
+				</a>
 
-				<amp-img
-					alt="Mountains"
-					width="550"
-					height="368"
-					layout="responsive"
-					src="https://amp.dev/static/inline-examples/images/mountains.webp"
-				></amp-img>
+				{isAmp && (
+					<amp-img
+						alt="Mountains"
+						width="550"
+						height="368"
+						layout="responsive"
+						src="https://amp.dev/static/inline-examples/images/mountains.webp"
+					></amp-img>
+				)}
+				
+				{!isAmp && (
+					<div>
+						<img
+							alt="Mountains"
+							width="550"
+							height="368"
+							src="https://amp.dev/static/inline-examples/images/mountains.webp"
+						/>
+					</div>
+				)}
+
 				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium aliquid eaque ducimus iste cupiditate distinctio minima tenetur deleniti, suscipit quod! Laudantium nostrum porro, odio enim aut repellat? Amet, impedit iste.</p>
 			</main>
 			<style jsx>{`
